ec3b74f991c296c102eed5dfeba900ec964f80bf
azanniel <leo.azannielttt@gmail.com>
2022-04-12 11:29:08 -0400

    feat: add car item props

--- src/components/CarItem/index.tsx
+++ src/components/CarItem/index.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
-import {ImageBackground, Text, View} from 'react-native';
+import {ImageBackground, ImageSourcePropType, Text, View} from 'react-native';
 import StyledButton from '../StyledButton';
 
 import styles from './styles';
 
-export default function CarItem() {
+type CarItemProps = {
+  name: string;
+  tagline: string;
+  taglineCTA?: string;
+  image: ImageSourcePropType;
+};
+
+export default function CarItem({
+  name,
+  tagline,
+  taglineCTA,
+  image,
+}: CarItemProps) {
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require('../../assets/ModelS.jpeg')}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
 
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline} <Text style={styles.subtitleCta}>{taglineCTA}</Text>
+        </Text>
       </View>
 
-      <StyledButton
-        type="primary"
-        content="Custom Order"
-        onPress={() => {
-          console.warn('Custom order was pressed');
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content="Custom Order"
+          onPress={() => {
+            console.warn('Custom order was pressed');
+          }}
+        />
 
-      <StyledButton
-        type="secondary"
-        content="Existing Inventory"
-        onPress={() => {
-          console.warn('Existing Inventory was pressed');
-        }}
-      />
+        <StyledButton
+          type="secondary"
+          content="Existing Inventory"
+          onPress={() => {
+            console.warn('Existing Inventory was pressed');
+          }}
+        />
+      </View>
     </View>
   );
 }
